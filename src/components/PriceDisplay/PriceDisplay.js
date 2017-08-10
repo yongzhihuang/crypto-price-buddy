3b59b213e0db060f3e624a2173d3979abb7b6eda
yongzhihuang <yongzhihuang@yahoo.com>
2017-08-09 22:20:50 -0400

    moved daily stat to top

--- src/components/PriceDisplay/PriceDisplay.js
+++ src/components/PriceDisplay/PriceDisplay.js
@@ -55,8 +55,8 @@ class PriceDisplay extends Component {
           <option value="ltc-eur">ltc-eur</option>
         </select>
 
-        <div className="price"><a href="https://www.gdax.com/trade/ETH-USD" target="_blank" rel="noopener noreferrer">${this.state.price}</a></div>
         <DailyStats />
+        <div className="price"><a href="https://www.gdax.com/trade/ETH-USD" target="_blank" rel="noopener noreferrer">${this.state.price}</a></div>
 
         <AskBids />
         <PriceTarget />
