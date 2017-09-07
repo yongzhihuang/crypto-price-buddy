1c673338dd5499e086ea83782797fed9222eb059
yongzhihuang <yongzhihuang@yahoo.com>
2017-09-06 20:40:52 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -55,10 +55,10 @@ class App extends Component {
 
         <PriceDisplay currency={this.state.currencyType} />
         <TopCrypto />
+        <iframe width="100%" height="250" src="https://www.youtube.com/embed/OT5iufs218g" frameborder="0" allowfullscreen></iframe>
         <LatestNews />
 
         <div className="about">
-          <iframe width="100%" height="250" src="https://www.youtube.com/embed/OT5iufs218g" frameborder="0" allowfullscreen></iframe>
           <p>Donate ETH: 0x79aCad1654fd277096571E536b9Bd49b33c024e4</p>
           <p>Don't forget to add us to home screen</p>
           <p>Made by <a href="https://www.twitter.com/whyzhi">@whyzhi</a></p>
